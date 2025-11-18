6dacbd0410e694c4a33669927326c578727d27c8
shiuly-28 <shulybd1245@gmail.com>
2025-11-18 10:48:06 +0600

    new create the product component

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
+import Product from './components/Product'
 
 export default function page() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Product/>
       <Footer/>
     </div>
   )
